c574e85554fd62f0a41c6a608c13debdfbf8ade3
Your Name <you@example.com>
2025-10-29 16:48:07 +0100

    gotowa strona - dodanie ikonek, stylizacja tekstu

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 'use strict';
 
 // zmienne przechowujące klasy/ID elementów html
-const glowneMenu = document.getElementsByClassName('menuGlowne');
-const daneOsobiste = document.getElementsByClassName('daneOsobiste');
-const profilZawodowy = document.getElementsByClassName('profilZawodowy');
-const doswiadczenie = document.getElementsByClassName('doswiadczenie');
-const profesionalnyProfil = document.getElementsByClassName('profesionalnyProfil');
-const wyksztalcenie = document.getElementsByClassName('wyksztalcenie');
-const Umiejetnosci = document.getElementsByClassName('umiejetnosci');
+const glowneMenu = document.getElementById('menuGlowne');
+const profil = document.getElementById('profil');
+const doswiadczenie = document.getElementById('doswiadczenie');
+const wyksztalcenie = document.getElementById('wyksztalcenie');
+const umiejetnosci = document.getElementById('umiejetnosci');
 
-function changeMainWindow() {
+function changeDisplayedWindow(showElementID) {
+    glowneMenu.classList.remove('hidden');
     glowneMenu.classList.add('hidden');
-    daneOsobiste.classList.add('hidden');
-    profilZawodowy.classList.add('hidden');
+
+    profil.classList.remove('hidden');
+    profil.classList.add('hidden');
+
+    doswiadczenie.classList.remove('hidden');
     doswiadczenie.classList.add('hidden');
-    profesionalnyProfil.classList.add('hidden');
+
+    wyksztalcenie.classList.remove('hidden');
     wyksztalcenie.classList.add('hidden');
-    Umiejetnosci.classList.add('hidden');
+
+    umiejetnosci.classList.remove('hidden');
+    umiejetnosci.classList.add('hidden');
+
+    showElementID.classList.remove('hidden');
 }
